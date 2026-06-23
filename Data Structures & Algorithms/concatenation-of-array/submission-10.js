6cd108d86a8dcde0808bed3a11c5d9deafc478d6
kevbot00 <40771568+kevbot00@users.noreply.github.com>
2026-06-23 11:50:18 -1000

    Add: concatenation-of-array - submission-10

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-10.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+      let ans = new Array(nums.length * 2)
+      for ( let i = 0; i < nums.length; i++ ) {
+        ans[i] = nums[i]
+        ans[nums.length + i] = nums[i]
+      }
+
+      return ans
+    }
+}
